27225d2ab789f90291252d7dc5e73cb6ad4534ae
mahy <dsz9258@126.com>
2018-11-20 20:00:45 +0800

    Feat: init project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Provider store={store}>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            hello
         </header>
-      </div>
+      </Provider>
     );
   }
 }
 
-export default App;
+export default App
